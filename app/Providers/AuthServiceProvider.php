04ce5b784308d822a4f8586e1fb5d6ae856b338d
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-07-21 19:09:48 +0900

    gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,15 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        
+        Gate::define('admin',function($user){
+            return $user->role===1;
+        });
+        Gate::define('manager-higher',function($user){
+           return $user->role>0 && $user->role<=5;
+        });
+        Gate::define('user-higher',function($user){
+            return $user->role>0 && $user->role<=9;
+        });
     }
 }
